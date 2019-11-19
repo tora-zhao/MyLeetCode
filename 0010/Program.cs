7566ba6dcbfc7b742230631e22ca2519edf4f67d
tora.zhao <tora.zhao@okcoin.com>
2019-11-19 12:44:04 +0900

    10 & 11

---
+++ 0010/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+https://leetcode.com/problems/regular-expression-matching/
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+
+Example 5:
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false  */
+/* NOT SOLVED */
+namespace _0010
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsMatch("aaa", "a*a")); 
+            Console.WriteLine(IsMatch("aa", "a"));
+            Console.WriteLine(IsMatch("aa", "a*"));
+            Console.WriteLine(IsMatch("ab", ".*"));
+            Console.WriteLine(IsMatch("aab", "c*a*b"));
+            Console.WriteLine(IsMatch("aab", "c*a*ba*b*"));
+            Console.WriteLine(IsMatch("mississippi", "mis*is*p*."));
+            Console.WriteLine("aaa"[2..]);
+        }
+
+
+        /* 参考答案1: 递归 */
+        public static bool IsMatch(string s, string p)
+        {
+            if(string.IsNullOrEmpty(p)) 
+            {
+                return string.IsNullOrEmpty(s); 
+            }
+
+            bool firstMatch = (
+                !string.IsNullOrEmpty(s) 
+                && (s[0] == p[0] || p[0] == '.'));
+
+            if(p.Length >= 2 && p[1] == '*')
+            {
+                return (IsMatch(s, p[2..]) || (firstMatch && IsMatch(s[1..], p)));
+            }
+            else
+            {
+                return firstMatch && IsMatch(s[1..], p[1..]);
+            }
+        }
+
+
+        // /* 简化的无限自动状态机 */
+        // public class Status
+        // {
+        //     public char Symbol { get; }
+        //     public bool IsStar { get; private set; }
+        //     public List<Status>  nextStatus { get; private set;}
+
+        //     public Status(char symbol, bool isStar = false)
+        //     {
+        //         this.Symbol = symbol;
+        //         this.IsStar = isStar;
+        //     }
+
+
+        //     /* pattern解析器，将pattern抽象为一系列状态
+        //     例如： mis*is*p*.  将会解析为7个状态， m i s* i s* p* . */
+        //     public static List<Status> Parse(string p)
+        //     {
+        //         var sList = new List<Status>();
+        //         for (int i = 0; i < p.Length; i++)
+        //         {
+        //             if (p[i] == '*')
+        //             {
+        //                 // 处理连续出现*的情况
+        //                 if (i == 0 || p[i - 1] == '*')
+        //                 {
+        //                     throw new ArgumentException("Illegal pattern");
+        //                 }
+        //                 else
+        //                 {
+        //                     sList[sList.Count - 1].IsStar = true;
+        //                 }
+        //             }
+        //             else
+        //             {
+        //                 var s = new Status(p[i]);
+        //                 sList.Add(s);
+        //             }
+        //         }
+        //         return sList;
+        //     }
+        // }
+    }
+}
+
+
+/*
+            Console.WriteLine(IsMatch("aaa", "a* a")); 
+            Console.WriteLine(IsMatch("aa", "a"));
+            Console.WriteLine(IsMatch("aa", "a*"));
+            Console.WriteLine(IsMatch("ab", ".*"));
+            Console.WriteLine(IsMatch("aab", "c*a* b"));
+            Console.WriteLine(IsMatch("aab", "c*a* b a*b*"));
+            Console.WriteLine(IsMatch("mississippi", "1:m 2:i 3:s* 4:i 5:s* 6:p* 7:."));
+
+            
+            {1}  {2} {3, 4}  {4, 5} -> {5, 6, 7} -> {accept}
+ */
---
+++ 0011/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/*
+https://leetcode.com/problems/container-with-most-water/
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+Find two lines, which together with x-axis forms a container, 
+such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2. */
+namespace _0011
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxArea2(new int[] {1,8,6,2,5,4,8,3,7}));
+        }
+
+        /* 穷举法 */
+        public static int MaxArea(int[] height)
+        {
+            int max = 0;
+
+            for(var i = 0; i < height.Length; i++)
+            {
+                for(var j = i; j < height.Length; j++)
+                {
+                    int area = (j-i) * Math.Min(height[i], height[j]);
+                    max = area > max ? area : max;
+                }
+            }
+            return max;
+        }
+
+
+        /* 两端逼近法： 
+        1.两指针分别指向首尾
+        2.比较首尾处的值，将较小的一方的指针向中间移动一位
+        3.重复2，直到两指针重合
+        关于该算法正确性的基于矩阵的证明: 
+        https://leetcode.com/problems/container-with-most-water/discuss/6099/yet-another-way-to-see-what-happens-in-the-on-algorithm
+        ps: 因为不明白数学原理，所以考虑出出该方法。*/
+        public static int MaxArea2(int[] height)
+        {
+            int i = 0, j = height.Length - 1, maxArea = 0;
+            while(i < j)
+            {
+                if(height[i] <= height[j])
+                {
+                    var area = height[i] * (j-i);
+                    maxArea = area > maxArea ? area : maxArea;
+                    i++;
+                }
+                else
+                {
+                    var area = height[j] * (j-i);
+                    maxArea = area > maxArea ? area : maxArea;
+                    j--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
